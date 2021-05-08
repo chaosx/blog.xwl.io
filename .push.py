76d4245da21151b1500b5a1277f5446468ca44c5
Xu, Wenlong <chaos@csmzxy.org>
2021-05-08 20:45:06 +0800

    Create .push.py

---
+++ .push.py
@@ -0,0 +1,19 @@
+import re
+import json
+import requests
+
+res = requests.get("https://blog.xwl.io/archives/")
+
+tags = re.findall(r'href="([a-zA-z]+://[^\s]*)"', str(res.text))
+
+urls = []
+for t in tags:
+    if t.endswith("css") or t.endswith("js"):continue
+    if not t.startswith("https://blog.xwl.io"):continue
+    urls.append(t)
+
+urls = "\n".join(urls)
+
+headers = {"Content-Type": "text/plain"}
+resp = requests.post("http://data.zz.baidu.com/urls?site=https://blog.xwl.io&token=ZMBvO1Tb3IDz4XUs", headers=headers, data=urls)
+print(resp.text)
